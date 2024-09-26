3ca8c71a72e1fc42bce2b6e9dd4a67307f151e01
mtsnatalino <matheuspedro982@gmail.com>
2024-09-26 14:02:47 -0300

    petlife

---
+++ index.js
@@ -0,0 +1,7 @@
+let elementosDuvida = document.querySelectorAll('.duvida');
+
+elementosDuvida.forEach(function (duvida){
+duvida.addEventListener('click', function () {
+    duvida.classList.toggle('ativa')
+})
+})
